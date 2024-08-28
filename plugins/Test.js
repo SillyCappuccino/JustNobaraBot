24e1baf198cf1996decd782dc296aa8e3c9009d0
SillyCappuccino <alguienxd510@gmail.com>
2024-08-28 00:49:36 -0300

    Update Test.js

--- plugins/Test.js
+++ plugins/Test.js
@@ -6,8 +6,7 @@ let MessageType = (await import(global.baileys)).default
 let handler = async (m, {usedPrefix, command, conn}) => {
 let frep = { contextInfo: { externalAdReply: {title: wm, body: lenguajeGB.smsCont18Porn2(), sourceUrl: redesMenu.getRandom(), thumbnail: await(await fetch(img16)).buffer() }}}
 let user = global.db.data.users[m.sender]
-  
-try {   
+    
 if (command == "test" || command "test2") {
 let res = (await axios.get(`https://github.com/SillyCappuccino/JustNobaraBot/blob/master/src/JSON/Slaps.js`)).data  
 let enlace = await res[Math.floor(res.length * Math.random())]
