ba2c9368dfcfdb1365c308755102fd020a465940
SillyCappuccino <alguienxd510@gmail.com>
2024-08-28 00:58:23 -0300

    Update Test.js

--- plugins/Test.js
+++ plugins/Test.js
@@ -11,7 +11,7 @@ try {
 if (command == 'test' || command == 'test2') {
 let res = (await axios.get(`https://github.com/SillyCappuccino/JustNobaraBot/blob/master/src/JSON/Slaps.js`)).data  
 let enlace = await res[Math.floor(res.length * Math.random())]
-  await conn.sendFile(m.chat, enlace, null, `, `+${m.sender.split('@')[0]} le dio una bofetada a ${m.mentionedJid.map((user)=>(user === m.sender)? 'alguien ': `+${user.split('@')[0]}`).join(', ')}``, null, null, {viewOnce: false}, m)}
+await conn.sendFile(m.chat, enlace, null, `+${m.sender.split('@')[0]} le dio una bofetada a ${m.mentionedJid.map((user)=>(user === m.sender)? 'alguien ': `+${user.split('@')[0]}`).join(', ')}`, null, null, {viewOnce: false}, m)}
 //if(m.quoted?.sender) m.mentionedJid.push(m.quoted.sender)
 //if(!m.mentionedJid.length) m.mentionedJid.push(m.sender)
 //let stiker = await sticker(null,s[Math.floor(Math.random() * s.length)], `+${m.sender.split('@')[0]} le dio una bofetada a ${m.mentionedJid.map((user)=>(user === m.sender)? 'alguien ': `+${user.split('@')[0]}`).join(', ')}`)
