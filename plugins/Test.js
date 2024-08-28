372e70718720cafb9a1ad85f3d14f4252ac2cdc8
SillyCappuccino <alguienxd510@gmail.com>
2024-08-28 00:52:18 -0300

    Update Test.js

--- plugins/Test.js
+++ plugins/Test.js
@@ -4,14 +4,14 @@ import fetch from 'node-fetch'
 
 let MessageType = (await import(global.baileys)).default
 let handler = async (m, {usedPrefix, command, conn}) => {
-    let frep = { contextInfo: { externalAdReply: {title: wm, body: lenguajeGB.smsCont18Porn2(), sourceUrl: redesMenu.getRandom(), thumbnail: await(await fetch(img16)).buffer() }}}
+//let frep = { contextInfo: { externalAdReply: {title: wm, body: lenguajeGB.smsCont18Porn2(), sourceUrl: redesMenu.getRandom(), thumbnail: await(await fetch(img16)).buffer() }}}
 let user = global.db.data.users[m.sender]
-    
-if (command == "test" || command "test2") {
+
+try {
+    if (command == "test" || command "test2") {
 let res = (await axios.get(`https://github.com/SillyCappuccino/JustNobaraBot/blob/master/src/JSON/Slaps.js`)).data  
 let enlace = await res[Math.floor(res.length * Math.random())]
   await conn.sendFile(m.chat, enlace, null, `, `+${m.sender.split('@')[0]} le dio una bofetada a ${m.mentionedJid.map((user)=>(user === m.sender)? 'alguien ': `+${user.split('@')[0]}`).join(', ')}``, null, null, {viewOnce: false}, m)}
-  
 //if(m.quoted?.sender) m.mentionedJid.push(m.quoted.sender)
 //if(!m.mentionedJid.length) m.mentionedJid.push(m.sender)
 //let stiker = await sticker(null,s[Math.floor(Math.random() * s.length)], `+${m.sender.split('@')[0]} le dio una bofetada a ${m.mentionedJid.map((user)=>(user === m.sender)? 'alguien ': `+${user.split('@')[0]}`).join(', ')}`)
