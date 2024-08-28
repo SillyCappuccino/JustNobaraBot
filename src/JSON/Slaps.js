c06dedb6d662509c2ac1dd572a01a66acdf09161
SillyCappuccino <alguienxd510@gmail.com>
2024-08-28 00:35:16 -0300

    Update Slaps.js

--- src/JSON/Slaps.js
+++ src/JSON/Slaps.js
@@ -1,1 +1,8 @@
-
+[
+"https://media.tenor.com/XiYuU9h44-AAAAAC/anime-slap-mad.gif",
+"https://img.photobucket.com/albums/v639/aoie_emesai/100handslap.gif",
+"https://gifdb.com/images/high/yuruyuri-akari-kyoko-anime-slap-fcacgc0edqhci6eh.gif",
+"https://gifdb.com/images/file/anime-sibling-slap-ptjipasdw3i3hsb0.gif",
+"https://c.tenor.com/Lc7C5mLIVIQAAAAC/tenor.gif",
+"https://i.pinimg.com/originals/71/a5/1c/71a51cd5b7a3e372522b5011bdf40102.gif"
+]
