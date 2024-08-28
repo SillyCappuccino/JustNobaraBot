355d29dc91e3ba133aee6be6cec28fcaac592526
SillyCappuccino <alguienxd510@gmail.com>
2024-08-28 01:12:57 -0300

    Update sticker-slap.js

--- plugins/sticker-slap.js
+++ plugins/sticker-slap.js
@@ -16,7 +16,12 @@ await conn.sendFile(m.chat, enlace, null, `+${m.sender.split('@')[0]} le dio una
 //if(!m.mentionedJid.length) m.mentionedJid.push(m.sender)
 //let stiker = await sticker(null,s[Math.floor(Math.random() * s.length)], `+${m.sender.split('@')[0]} le dio una bofetada a ${m.mentionedJid.map((user)=>(user === m.sender)? 'alguien ': `+${user.split('@')[0]}`).join(', ')}`)
 //conn.sendFile(m.chat, stiker, null, { asSticker: true })
-} catch (e) { }}
+} catch (e) {
+  await conn.reply(m.chat, `${lenguajeGB['smsMalError3']()}#report ${lenguajeGB['smsMensError2']()} ${usedPrefix + command}\n\n${wm}`, fkontak, m)
+console.log(`❗❗ ${lenguajeGB['smsMensError2']()} ${usedPrefix + command} ❗❗`)
+console.log(e)}
+}
+
 handler.help = ['slapss']
 handler.tags = ['General']
 handler.command = /^slap|test2|test3|abofetear|golpear/i
@@ -45,11 +50,11 @@ export default  handler
 //handler.command = /^slap|bofetada|manotada|abofetear|golpear/i
 //export default handler
 
-//const s = [
-//"https://media.tenor.com/XiYuU9h44-AAAAAC/anime-slap-mad.gif",
-//"https://img.photobucket.com/albums/v639/aoie_emesai/100handslap.gif",
-//"https://gifdb.com/images/high/yuruyuri-akari-kyoko-anime-slap-fcacgc0edqhci6eh.gif",
-//"https://gifdb.com/images/file/anime-sibling-slap-ptjipasdw3i3hsb0.gif",
-//"https://c.tenor.com/Lc7C5mLIVIQAAAAC/tenor.gif",
-//"https://i.pinimg.com/originals/71/a5/1c/71a51cd5b7a3e372522b5011bdf40102.gif"
-//]
+const s = [
+"https://media.tenor.com/XiYuU9h44-AAAAAC/anime-slap-mad.gif",
+"https://img.photobucket.com/albums/v639/aoie_emesai/100handslap.gif",
+"https://gifdb.com/images/high/yuruyuri-akari-kyoko-anime-slap-fcacgc0edqhci6eh.gif",
+"https://gifdb.com/images/file/anime-sibling-slap-ptjipasdw3i3hsb0.gif",
+"https://c.tenor.com/Lc7C5mLIVIQAAAAC/tenor.gif",
+"https://i.pinimg.com/originals/71/a5/1c/71a51cd5b7a3e372522b5011bdf40102.gif"
+]
