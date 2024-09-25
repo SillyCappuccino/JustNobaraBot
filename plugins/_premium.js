0b4d1527005a949fcfb2bf087959b3027b689f13
SillyCappuccino <alguienxd510@gmail.com>
2024-09-25 03:43:59 -0300

    Update _premium.js

--- plugins/_premium.js
+++ plugins/_premium.js
@@ -1,10 +1,10 @@
-let handler = m => m
+let handler = m => m;
+
 export async function all(m) {
-let user = global.db.data.users[m.sender]
-if (m.chat.endsWith('broadcast')) return
-if (user.premiumTime != 0 && user.premium) {
-if (new Date() * 1 >= user.premiumTime) {
-await m.reply(`${lenguajeGB['smsAvisoAG']()}*@${m.sender.split`@`[0]}* ${lenguajeGB['smsPremI']()}`, false, { mentions: [m.sender] })
-user.premiumTime = 0
-user.premium = true
-}}}
+    let user = global.db.data.users[m.sender];
+    
+    if (!user || m.chat.endsWith('broadcast')) return;
+
+    // Aquí puedes agregar cualquier lógica que necesites
+    // sin referencia al sistema premium.
+}
