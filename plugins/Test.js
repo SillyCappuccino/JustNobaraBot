7d02f05b8a984b046bea11be036785ae2e16c991
SillyCappuccino <alguienxd510@gmail.com>
2024-09-25 03:49:26 -0300

    Update Test.js

--- plugins/Test.js
+++ plugins/Test.js
@@ -1,24 +1,29 @@
 import axios from "axios"
-import { sticker } from '../lib/sticker.js'
 import fetch from 'node-fetch'
 
-let MessageType = (await import(global.baileys)).default
-let handler = async (m, {usedPrefix, command, conn}) => {
-//let frep = { contextInfo: { externalAdReply: {title: wm, body: lenguajeGB.smsCont18Porn2(), sourceUrl: redesMenu.getRandom(), thumbnail: await(await fetch(img16)).buffer() }}}
-let user = global.db.data.users[m.sender]
+let handler = async (m, { conn }) => {
+    let user = global.db.data.users[m.sender]
+    
+    try {
+        if (['test', 'test2', 'test3', 'abofetear', 'golpear'].includes(m.command)) {
+            // Obtiene imágenes desde el repositorio de GitHub
+            let res = (await axios.get(`https://github.com/SillyCappuccino/JustNobaraBot/blob/master/src/JSON/Slaps.js`)).data  
+            let enlace = await res[Math.floor(res.length * Math.random())];
 
-try {
-if (command == 'test' || command == 'test2') {
-let res = (await axios.get(`https://github.com/SillyCappuccino/JustNobaraBot/blob/master/src/JSON/Slaps.js`)).data  
-let enlace = await res[Math.floor(res.length * Math.random())]
-await conn.sendFile(m.chat, enlace, null, `+${m.sender.split('@')[0]} le dio una bofetada a ${m.mentionedJid.map((user)=>(user === m.sender)? 'alguien ': `+${user.split('@')[0]}`).join(', ')}`, null, null, {viewOnce: false}, m)}
-//if(m.quoted?.sender) m.mentionedJid.push(m.quoted.sender)
-//if(!m.mentionedJid.length) m.mentionedJid.push(m.sender)
-//let stiker = await sticker(null,s[Math.floor(Math.random() * s.length)], `+${m.sender.split('@')[0]} le dio una bofetada a ${m.mentionedJid.map((user)=>(user === m.sender)? 'alguien ': `+${user.split('@')[0]}`).join(', ')}`)
-//conn.sendFile(m.chat, stiker, null, { asSticker: true })
-} catch (e) { }}
+            const mentionedUsers = m.mentionedJid.map((jid) => jid.split('@')[0]).join(', ');
+            const senderName = m.sender.split('@')[0]; // Obtiene el nombre del usuario que dio la bofetada
+            
+            // Envío del GIF y mensaje
+            await conn.sendFile(m.chat, enlace, null, `${senderName} le dio una bofetada a ${mentionedUsers}`, null, null, { viewOnce: false }, m);
+        }
+    } catch (e) {
+        console.error(e); // Para facilitar el diagnóstico de errores, si los hay
+    }
+}
+
 handler.help = ['slapss']
 handler.tags = ['General']
-handler.command = /^test|test2|test3|abofetear|golpear/i
+handler.command = /^(test|test2|test3|abofetear|golpear)$/i
 handler.register = true
-export default  handler
+
+export default handler
