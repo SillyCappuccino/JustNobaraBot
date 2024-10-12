77dbffd148213044c4d21bf0d677f07e8b092571
SillyCappuccino <alguienxd510@gmail.com>
2024-10-11 22:46:16 -0300

    Delete adasads.js

--- adasads.js
+++
@@ -1,9 +0,0 @@
-let handler = async (m) => {
-    if (m.text.startsWith('!skibidi')) {
-        console.log('Comando recibido: !skibidi'); // Verificación
-        await conn.reply(m.chat, '¡Comando test ejecutado correctamente!', m);
-    }
-}
-
-handler.command = /^skibidi/i; // Comando registrado
-export default handler;
